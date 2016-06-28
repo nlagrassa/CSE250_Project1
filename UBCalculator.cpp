ce6296271c0d69a71a7dcb61f68fc55d2e0a500a
nicholaslagrassa <nlagrassa@gmail.com>
2016-06-28 01:36:20 -0400

    Stuck on accessing precedenceMap to set opStack push and pop conditions

--- UBCalculator.cpp
+++ UBCalculator.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+void checkForErrors(Token &, stack<Token> &, stack<Token> &, vector<Token> &);
 
 const std::map<char, char> UBCalculator::delimMatch = UBCalculator::createDelimMatch();
 const  std::map<char, int> UBCalculator::precedenceMap = UBCalculator::createPrecedenceMap();
@@ -15,61 +16,72 @@ const std::string UBCalculator::closeDelims = ")]}";
 
 
 // Evaluate str 
-void UBCalculator::setLine(std::string str)
-{
- 
-  //read each token for its value and put them in the approp stacks
-  //evaluate the expression as either a result or an error
-  //output the result or error
+void UBCalculator::setLine(std::string str) {
 
-  //Needed: map, vector, stack
+    //read each token for its value and put them in the approp stacks
+    //evaluate the expression as either a result or an error
+    //output the result or error
 
-  ///make a lexer
-  Lexer lex(str);
-  ///tokenize lexer
-  vector<Token> tokenVec = lex.tokenize();
+    //Needed: map, vector, stack
 
-  ///read each token for its value and put htem int he approp stacks
-  stack<Token> valueStack;				//stack declarations
-  stack<Token> opStack;
- 
-  for(size_t i=0; i<tokenVec.size();i++){	//loop through vector and sort tokens by type
-     
-    bool tokenIsOperator = tokenVec[i].type == OPERATOR;
-    bool tokenIsOpenDelim = (openDelims.find(tokenVec[i].value)!=string::npos);
-    bool tokenIsCloseDelim = (closeDelims.find(tokenVec[i].value)!=string::npos);
-    bool tokenIsValue = tokenVec[i].type == NUMBER;
+    ///make a lexer
+    Lexer lex(str);
+    ///tokenize lexer
+    vector<Token> tokenVec = lex.tokenize();
 
-    if(tokenVec[i].type == OPERATOR || tokenVec[i].type == DELIM){
-       
-       
+    ///read each token for its value and put htem int he approp stacks
+    stack<Token> numberStack;                //stack declarations
+    stack<Token> opStack;
 
-       
+    for (size_t i = 0; i < tokenVec.size(); i++) {    //loop through vector and sort tokens by type
 
-       if(opStack.empty()|| (openDelims.find(tokenVec[i].value)!=string::npos)){
-      
-       } 
+        bool tokenIsOperator = tokenVec[i].type == OPERATOR;
+        bool tokenIsOpenDelim = (openDelims.find(tokenVec[i].value) != string::npos);
+        bool tokenIsCloseDelim = (closeDelims.find(tokenVec[i].value) != string::npos);
+        bool tokenIsNumber = tokenVec[i].type == NUMBER;
+        bool tokenIsIdent = tokenVec[i].type == IDENT;
+        Token thisToken = tokenVec[i];
 
-       else{
-         
-       }
- 
-      // opStack.push(tokenVec[i]);
+
+        //IF TOKEN IS A NUMBER
+        if(tokenIsNumber){
+
+            numberStack.push(thisToken);
+        }
+
+        if(tokenIsOperator){
+            if(opStack.empty()&&tokenIsCloseDelim){
+                //Expression not well formed error goes here
+            }
+            else if(opStack.empty()&&!tokenIsCloseDelim){
+                opStack.push(thisToken);
+            }
+            else if (opStack.top().value=="=" || (openDelims.find(opStack.top().value)!= string::npos)){
+                opStack.push(thisToken);
+            }
+            else{
+                while(precedenceMap.at(thisToken.value) <= precedenceMap.at(opStack.top().value)){
+
+                }
+            }
+
+
+        }
+
+        if(tokenIsIdent){
+
+        }
+
+
 
     }
 
-    if(tokenVec[i].type == IDENT || tokenVec[i].type == NUMBER){
 
-       valueStack.push(tokenVec[i]);
+}
 
-    } 
+void checkForErrors(Token &thisToken, stack<Token> &numberStack, stack<Token> &opStack, vector<Token> &tokenVec){
 
-  }  
-
-  cout << "Operators: " << opStack.size() << endl;
-  cout << "Identifiers and values: " << valueStack.size() << endl; 
+    //INSERT ALL POSSIBLE ERROR CASES HERE
 
 
-
-   return;
 }
