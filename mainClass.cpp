24ebcb13305174e5014624e5706a4e4bebccda4a
nicholaslagrassa <nlagrassa@gmail.com>
2016-06-27 23:25:46 -0400

    Added CMakeLists.txt file

---
+++ mainClass.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Nicholas LaGrassa on 6/27/16.
+//
+
+#include "mainClass.h"
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+    cout<<"This works" << endl;
+
+    return 0;
+
+}
---
+++ mainClass.h
@@ -0,0 +1,14 @@
+//
+// Created by Nicholas LaGrassa on 6/27/16.
+//
+
+#ifndef CSE250_PROJECT1_MAINCLASS_H
+#define CSE250_PROJECT1_MAINCLASS_H
+
+
+class mainClass {
+
+};
+
+
+#endif //CSE250_PROJECT1_MAINCLASS_H
